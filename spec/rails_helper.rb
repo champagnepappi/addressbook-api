8896f4b6c91a8f2cbf5a535b545e7fe310673e6d
Kevin Santos <kevinochieng548@gmail.com>
2019-04-10 16:58:43 +0300

    add swagger helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,8 @@ config.around(:each) do |example|
   end
 end
 
+config.backtrace_inclusion_patterns = [%r{app|spec}]
+
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
