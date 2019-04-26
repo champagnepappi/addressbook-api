86cbac1544d3e8cca556c63e89bf1711f93e4a13
Kevin Santos <kevinochieng548@gmail.com>
2019-04-26 19:04:13 +0300

    move database cleaner to the config block

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,19 +24,6 @@ require 'factory_bot_rails'
 #
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
-config.before(:suite) do
-  DatabaseCleaner.strategy = :transaction
-  DatabaseCleaner.clean_with(:truncation)
-end
-
-config.around(:each) do |example|
-  DatabaseCleaner.cleaning do
-    example.run
-  end
-end
-
-config.backtrace_inclusion_patterns = [%r{app|spec}]
-
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
@@ -53,6 +40,18 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  config.backtrace_inclusion_patterns = [%r{app|spec}]
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
