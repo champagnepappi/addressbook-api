66969c177d1244e0d1771d6876a96d530e22da77
Kevin Santos <kevinochieng548@gmail.com>
2019-04-26 18:50:27 +0300

    define the create method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,16 @@
 class ContactsController < ApplicationController
   def index
-      response = Contact.all
-      render json: response, status: :ok
+    response = Contact.all
+    render json: response, status: :ok
+  end
+
+  def create
+    response = Contact.create(contact_params)
+    render json: response, status: :created
+  end
+
+  private
+  def contact_params
+    params.require(:data).permit(:name, :phone, :email, :address)
   end
 end
