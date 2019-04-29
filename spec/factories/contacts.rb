628363fbc3a10ae66915cb464c2d6ed3afe242a2
Kevin Santos <kevinochieng548@gmail.com>
2019-04-29 17:03:03 +0300

    modify the contacts factory

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,16 +1,8 @@
 FactoryBot.define do
   factory :contact do
-    sequence :name do |n|
-      Faker::Name.name                                                                 0
-    end
     phone { rand(10**9...10**10) }
-
-    sequence :email do |n|
-      Faker::Internet.email
-    end
-
-    sequence :address do |n|
-      Faker.Address.city
-    end
+    name { Faker::Name.first_name }
+    email  { Faker::Internet.email }
+    address { Faker::Address.city }
   end
 end
